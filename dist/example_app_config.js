e42a9f9a0249a9dd91e4a195af31c784cca92a6c
seraph-coder-1229 <seraph1229@protonmail.com>
2025-06-15 18:33:38 -0700

    dist upload

---
+++ dist/example_app_config.js
@@ -0,0 +1,75 @@
+window.UPSHOT_USER_APP_CONFIG = {
+    "version": 1,
+    "positionSettings": {
+      "PILOT": {
+        "useDerivedLevels": true,
+        "deadlines": {
+          "200": {
+            "targetMonths": 6,
+            "deadlineMonths": 12
+          },
+          "300": {
+            "targetMonths": 10,
+            "deadlineMonths": 18
+          },
+          "400": {
+            "targetMonths": 12,
+            "deadlineMonths": 24
+          }
+        }
+      },
+      "NFO": {
+        "useDerivedLevels": true,
+        "deadlines": {
+          "200": {
+            "targetMonths": 5,
+            "deadlineMonths": 10
+          },
+          "300": {
+            "targetMonths": 9,
+            "deadlineMonths": 16
+          },
+          "400": {
+            "targetMonths": 11,
+            "deadlineMonths": 22
+          }
+        }
+      },
+      "AAW": {
+        "useDerivedLevels": true,
+        "deadlines": {
+          "200": {
+            "targetMonths": 7,
+            "deadlineMonths": 14
+          },
+          "300": {
+            "targetMonths": 8,
+            "deadlineMonths": 18
+          }
+        }
+      },
+      "EWO": {
+        "useDerivedLevels": true,
+        "deadlines": {
+          "200": {
+            "targetMonths": 6,
+            "deadlineMonths": 13
+          },
+          " 300": {
+            "targetMonths": 10,
+            "deadlineMonths": 19
+          }
+        }
+      }
+    },
+    "useRoundedTrainingStartDate": true,
+    "trainingDepartment": {
+      "squadron": "YOUR SQUADRON",
+      "departmentHead": "Dept Head Name",
+      "pilotLead": "Pilot Lead Name",
+      "nfoLead": "NFO Lead Name",
+      "aawLead": "AAW Lead Name",
+      "ewoLead": "EWO Lead Name"
+    },
+    "geminiApiKey": ""
+  };
